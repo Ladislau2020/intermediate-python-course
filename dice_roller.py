62e37ffa9caa204c781f6c0f1fb87be3a983d603
Ladislau2020 <75049562+Ladislau2020@users.noreply.github.com>
2022-06-27 21:56:17 +0100

    little update
    I have added player name and keep his total score

--- dice_roller.py
+++ dice_roller.py
@@ -3,8 +3,10 @@
 def main():
     # importing the randon package to help us generating random numbers
     import random
+    player = str(input('Say your name: '))
     dice_rolls = int(input('How many dice would you like to roll? '))
-    dice_size = int(input('How many sides are the dice? ')) 
+    dice_size = int(input('How many sides are the dice? '))
+
     dice_sum = 0
     for i in range(0, dice_rolls):
         roll = random.randint(1,dice_size)
@@ -15,7 +17,9 @@ def main():
             print(f'You rolled a {roll}! Critical Sucess')
         else:
             print(f'You rolled a {roll}')
-    print(f'You have rolled a total of {dice_sum}')
+    # print(f'You have rolled a total of {dice_sum}') i think i dont need this anymore
+    player_total = f'{player} had  {dice_sum}'
+    print(player_total)
 
 if __name__== "__main__":
     main()
